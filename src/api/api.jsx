c033e06faa1d39a141788bc228c81658b5db340a
Joseph Muentes <muentes2@hotmail.com>
2024-05-14 15:54:45 -0400

    api

--- src/api/api.jsx
+++ src/api/api.jsx
@@ -3,12 +3,12 @@ import axios from 'axios';
 
 
 export const login = async (email, password) => {
-    const response = await axios.post("/api/login", { email: email, password: password });     
+    const response = await axios.post("http://epp3.ovh:5000/api/login", { email: email, password: password });     
     return response;
   };
   
   export const getMyEvaluation = async (userId,token) => {
-      const response = await axios.get("/api/my-evaluations", 
+      const response = await axios.get("http://epp3.ovh:5000/api/my-evaluations", 
       {
         params: {
         user_id: userId,
