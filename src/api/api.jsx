a2f92eba10899751c3f095134e1a34b868bb42bb
Joseph Muentes <muentes2@hotmail.com>
2024-05-14 16:16:27 -0400

    variables env

--- src/api/api.jsx
+++ src/api/api.jsx
@@ -1,14 +1,14 @@
 import axios from 'axios';
-//const backendUrl = process.env.REACT_APP_BACKEND_URL;
+const backendUrl = process.env.REACT_APP_BACKEND_URL;
 
 
 export const login = async (email, password) => {
-    const response = await axios.post("/api/login", { email: email, password: password });     
+    const response = await axios.post(`${backendUrl}/api/login`, { email: email, password: password });     
     return response;
   };
   
   export const getMyEvaluation = async (userId,token) => {
-      const response = await axios.get("/api/my-evaluations", 
+      const response = await axios.get(`${backendUrl}/api/my-evaluations`, 
       {
         params: {
         user_id: userId,
