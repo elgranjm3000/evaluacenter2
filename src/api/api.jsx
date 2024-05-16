567f701dd49804823ae6be1f4c52f9b95fdb4b7a
Joseph Muentes <muentes2@hotmail.com>
2024-05-16 19:48:59 -0400

    probando a ver

--- src/api/api.jsx
+++ src/api/api.jsx
@@ -1,5 +1,8 @@
 import axios from 'axios';
-//axios.defaults.baseURL = "http://epp3.ovh:5000";
+axios.defaults.baseURL = "https://cors-anywhere.herokuapp.com/http://epp3.ovh:5000";
+//axios.defaults.headers.common['Access-Control-Allow-Origin'] = '*';
+ //axios.defaults.headers.common['Content-Type'] = 'application/json';
+ 
 
 
 export const login = async (email, password) => {
