8dbaf8cb9faf6c5a959c95c6d490bea5a0a2623a
Joseph Muentes <muentes2@hotmail.com>
2024-05-16 16:18:21 -0400

    vite config

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:'https://evaluacenter2.vercel.app/',
-  server: {
-    host: true,
-    port : 3000,
+  base:'/',
+  server: {   
     proxy: {
         '/api': {
             target: 'http://epp3.ovh:5000',
