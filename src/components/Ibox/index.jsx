f71d49986010509b734d0563fec5fa1ff4d7a91b
Joseph Muentes <muentes2@hotmail.com>
2024-05-16 16:00:58 -0400

    eliminando paquetes

--- src/components/Ibox/index.jsx
+++ src/components/Ibox/index.jsx
@@ -1,10 +1,8 @@
 import React from 'react';
-import { useSelector, useDispatch } from 'react-redux';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCheck } from '@fortawesome/free-solid-svg-icons';
 
 const Ibox = ({ title, children, number }) => {
-  const dispatch = useDispatch();
   
   const handleOnPress = () => {
     console.log("hola");
