9a96f3f3d87ae76e78d304a669de80392de2ea36
Joseph Muentes <muentes2@hotmail.com>
2024-07-19 17:19:32 -0400

    Radares

--- src/components/test/radar/Radars.jsx
+++ src/components/test/radar/Radars.jsx
@@ -2,63 +2,39 @@ import React, { useState } from 'react';
 import CircularSlider from '@fseehawer/react-circular-slider';
 import { Typography } from '@mui/material';
 import { useMediaQuery, useTheme } from '@mui/material';
+import { RoundSlider } from 'mz-react-round-slider';
+
 
 const Radars = () => {
-  const labels = [0.5,1,1.5,2.5,3,3.5,4.5,5,5.5,6,6.5,7,7.5,8,8.5,9,9.5,10];
+  const labels = [0,0.5,1,1.5,2.5,3,3.5,4.5,5,5.5,6,6.5,7,7.5,8,8.5,9,9.5,10];
   const theme = useTheme();
-  const isMobile = useMediaQuery(theme.breakpoints.down('sm'));
-  const circularPickerNumber = [
-    { key: '1', number: '10/0', right: 101, top: 5 },
-    { key: '2', number: 0.5, right: 70, top: 10 },
-    { key: '3', number: 1, right: 45, top: 25 },
-    { key: '4', number: 1.5, right: 22, top: 47 },
-    { key: '5', number: 2, right: 10, top: 75 },
-    { key: '6', number: 2.5, right: 5, top: 104 },
-    { key: '7', number: 3, right: 10, top: 135 },
-    { key: '8', number: 3.5, right: 23, top: 160 },
-    { key: '9', number: 4, right: 47, top: 183 },
-    { key: '10', number: 4.5, right: 72, top: 198 },
-    { key: '11', number: 5, right: 107, top: 205 },
-    { key: '12', number: 5.5, right: 138, top: 198 },
-    { key: '13', number: 6, right: 168, top: 183 },
-    { key: '14', number: 6.5, right: 185, top: 160 },
-    { key: '15', number: 7, right: 205, top: 135 },
-    { key: '16', number: 7.5, right: 202, top: 104 },
-    { key: '17', number: 8, right: 205, top: 75 },
-    { key: '18', number: 8.5, right: 183, top: 47 },
-    { key: '19', number: 9, right: 170, top: 25 },
-    { key: '20', number: 9.5, right: 137, top: 10 },
-  ]
-  const [value, setValue] = useState(0);
+  const isMobile = useMediaQuery(theme.breakpoints.down('sm'));  
 
-  const radius = 200;
-  const center = radius / 2;
-  const labelRadius = radius / 2.2;
+  const [ pointers, setPointers ] = useState([
+    {
+        value: 0
+    }
+]);
 
   return (
     <div style={{ position: 'relative', display: 'flex', justifyContent: 'center', alignItems: 'center', height: '50h' }}>
-      <CircularSlider
-        width={isMobile ? 200 : 400} // Ancho del slider
-        label="Points"
-        min={0}
-        max={10}
-        dataIndex={2}
-        onChange={value => { setValue(value); }}
-        initialValue={0}
-        knobPosition="top" // Posición del botón
-        trackColor="#ddd" // Color de la pista
-        labelColor="#005a58"
-        knobColor="#005a58"
-        progressColorFrom="#00bfbd"
-        progressColorTo="#009c9a"
-        progressSize={10} // Tamaño del progreso      
-        knobSize={isMobile ? 24 : 32} // Tamaño del botón
-        appendToValue=" " // Agrega un espacio después del valor      
-        trackSize={8} // Tamaño de la pista
-        data={labels}
-
-      />
-      
+       <RoundSlider
+             min={0}
+             data={labels}
+             textColor={'#8993B7'}
+             textFontSize={24}
+             textFontFamily={'IBM Plex Sans'}
+             enableTicks={true}
+             showTickValues={true}
+             ticksGroupSize={1}            
+             pointers={pointers}
+             onChange={setPointers}
+             pathStartAngle={ 270 }
+             pathEndAngle={ 269.999 }
+             ticksColor={ '#efefef' }
+             tickValuesColor={ '#6093a3' }
+             textPrefix={ ' ' }
+        />
     
     </div>
   );
