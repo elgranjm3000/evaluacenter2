2af999ee594b0eb145fff76b66d90e4c4abad70c
Joseph Muentes <muentes2@hotmail.com>
2024-08-24 20:48:07 -0400

    sendData

--- src/App.jsx
+++ src/App.jsx
@@ -97,7 +97,7 @@ function App() {
    const data = { message: 'Hola desde la WebApp' };
 
       // Enviar datos usando Telegram WebApp API
-      window.Telegram.WebApp.sendData(JSON.stringify(data));
+      window.telegram.WebApp.WebApp.sendData(JSON.stringify(data));
   },[isLoggedIn])
 
   useEffect(() => {
