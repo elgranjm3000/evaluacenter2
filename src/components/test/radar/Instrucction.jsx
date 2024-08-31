2573336a491999c87fb7066700b2cb2ae80057f1
Joseph Muentes <muentes2@hotmail.com>
2024-08-30 23:20:52 -0400

    radar

--- src/components/test/radar/Instrucction.jsx
+++ src/components/test/radar/Instrucction.jsx
@@ -1,6 +1,6 @@
 import React, { Component,useState,useEffect } from 'react'; 
 
-import {  Typography,Fade } from '@mui/material';
+import {  Typography,Fade,useMediaQuery, useTheme } from '@mui/material';
 import { useTranslation } from 'react-i18next';
 import Box from '@mui/material/Box';
 import Card from '@mui/material/Card';
@@ -12,18 +12,24 @@ import people_rushing from '../../../assets/test/people-rushing.svg'
 import done from '../../../assets/test/done.svg'
 import asking_question from '../../../assets/test/asking-question.svg'
 import online_shopping from '../../../assets/test/online-shopping.svg'
+import styles from '../../styles';
+
 
 const Instrucction = () => { 
   
     const { t } = useTranslation();
     const [checked, setChecked] = useState(false);
+    const theme = useTheme();
+    const isMobile = useMediaQuery(theme.breakpoints.down('sm'));
+    const classes = styles(isMobile);
+
     const handleChange = (event) => {
         setChecked(event.target.checked);
       };
 return (
   <>
   <Fade in={true} timeout={1000}>
-            <Box style={{padding:"50px"}}>
+            <Box style={classes.pandingBox}>
   
             <Box borderBottom={1} borderColor="divider">
                             <Card>
@@ -63,11 +69,11 @@ return (
 
 
 
-            <FormControlLabel
+       <FormControlLabel
       control={<Checkbox checked={checked} onChange={handleChange} />}
       label="He leído y entendido cómo realizar la evaluación"
-      style={{color:"black", marginTop:'100px'}}
-    />
+      style={{color:"black", marginTop:'100px'}}/>
+
   </Box>
   </Fade> 
   </>
